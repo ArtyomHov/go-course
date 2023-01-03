3326801576dbdb909229c214bcb88f500622a49e
Artyom <artyom@ymail.com>
2023-01-03 13:16:57 +0400

    add chi router

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,14 +5,23 @@ import (
 
 	"github.com/ArtyomHov/go-course/pkg/config"
 	"github.com/ArtyomHov/go-course/pkg/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	// mux := pat.New()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(middleware.Logger)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
